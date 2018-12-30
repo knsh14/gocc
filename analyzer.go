4970689d91a3b5091165905adf1ac47761053f2f
kenshi kamata <kenshi.kamata@gmail.com>
2018-12-31 00:30:35 +0900

    create analyzer

---
+++ analyzer.go
@@ -0,0 +1,40 @@
+package gocc
+
+import (
+	"go/ast"
+
+	"github.com/knsh14/gocc/complexity"
+	"golang.org/x/tools/go/analysis/"
+	"golang.org/x/tools/go/analysis/passes/inspect"
+	"golang.org/x/tools/go/ast/inspector"
+)
+
+var (
+	threshold int
+	top       int
+	average   bool
+)
+
+var Analyzer = &analysis.Analyzer{
+	Name:     "gocc",
+	Doc:      "checks cyclomatic complexity",
+	Run:      run,
+	Requires: []*analysis.Analyzer{inspect.Analyzer},
+}
+
+func run(pass *analysis.Pass) (interface{}, error) {
+
+	inspect := pass.ResultOf[inspect.Analyzer].(*inspector.Inspector)
+	nodeFilter := []ast.Node{
+		(*ast.FuncDecl)(nil),
+	}
+
+	inspect.Preorder(nodeFilter, func(n ast.Node) {
+		count := complexity.Count(n)
+		if count >= 10 {
+			fd := n.(*ast.FuncDecl)
+			pass.Reportf(n.Pos(), "function %s complexity=%d", fd.Name.Name, count)
+		}
+	})
+	return nil, nil
+}
